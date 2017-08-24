a566b87d635b629126c7af79350b95085f20729c
Charlie Fish <fishcharlie@me.com>
2017-08-24 12:19:30 -0600

    Adding S3 upload code

---
+++ keys.js
@@ -0,0 +1,7 @@
+var keys = {
+	"s3bucket": "BUCKETNAME",
+	"s3accesskey": "ACCESSKEY",
+	"s3secretaccesskey": "SECRETACCESSKEY",
+};
+
+module.exports = keys;
--- server.js
+++ server.js
@@ -2,6 +2,22 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var fileUpload = require('express-fileupload');
+var s3 = require('s3');
+var keys = require('keys.js');
+ 
+var client = s3.createClient({
+  maxAsyncS3: 20,     // this is the default 
+  s3RetryCount: 3,    // this is the default 
+  s3RetryDelay: 1000, // this is the default 
+  multipartUploadThreshold: 20971520, // this is the default (20 MB) 
+  multipartUploadSize: 15728640, // this is the default (15 MB) 
+  s3Options: {
+    accessKeyId: keys.s3accesskey,
+    secretAccessKey: keys.s3secretaccesskey,
+    // any other options are passed to new AWS.S3() 
+    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property 
+  },
+});
 
 app.use(fileUpload());
 
@@ -22,7 +38,25 @@ app.post('/upload', function (req, res) {
     if (err) {
       return res.status(500).send(err);
     }
-    res.send('File uploaded!');
+    
+    // Upload to S3
+    var params = {
+      localFile: 'uploads/' + req.files.sampleFile.name,
+     
+      s3Params: {
+        Bucket: keys.s3bucket,
+        Key: req.files.sampleFile.name, // File path of location on S3
+      },
+    };
+    var uploader = client.uploadFile(params);
+    uploader.on('error', function(err) {
+      console.error("unable to upload:", err.stack);
+      res.status(500).send(err.stack);
+    });
+    uploader.on('end', function() {
+      console.log("done uploading");
+      res.send('File uploaded!');
+    });
   });
 });
 
