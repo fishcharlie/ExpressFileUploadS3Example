6ce995cbce4cc22e58cd7cb4a454b2b8085d1a13
Charlie Fish <fishcharlie@me.com>
2017-08-24 12:13:09 -0600

    Adding express file upload logic

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fileUpload = require('express-fileupload');
+
+app.use(fileUpload());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/views', 'index.html'));
+});
+
+app.post('/upload', function (req, res) {
+  if (!req.files) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
+  var sampleFile = req.files.sampleFile;
+
+  // Use the mv() method to place the file somewhere on your server 
+  sampleFile.mv('uploads/' + req.files.sampleFile.name, function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.send('File uploaded!');
+  });
 });
 
 app.listen(5000, function () {
---
+++ uploads/README.md
@@ -0,0 +1,1 @@
+This file was created so that the uploads/ folder would be checked into Git.
